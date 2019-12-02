b1cf59cff64c6da4bdaa30122c379d86a812d31b
Victor Pontis <victor@pont.is>
2019-12-01 21:34:05 -0800

    Solve day 1!

--- 1/1.ts
+++ 1/1.ts
@@ -1,18 +1,49 @@
 import { promises as fs } from "fs";
-import _ from 'lodash';
+import _ from "lodash";
 
-const main = async () => {
+const massToFuel = (mass: number): number => {
+  const fuel = Math.floor(mass / 3) - 2;
+  if (fuel <= 0) {
+    return 0;
+  }
+
+  return fuel;
+};
+
+const getRecursiveFuel = (mass: number): number => {
+  const fuel = massToFuel(mass);
+  if (fuel === 0) {
+    return 0;
+  }
+
+  return fuel + getRecursiveFuel(fuel);
+};
+
+const getMasses = async (): Promise<number[]> => {
   const fileHandle = await fs.open("1/input.txt", "r");
   const contentBuffer = await fileHandle.readFile();
   const content = contentBuffer.toString().trim();
-  const masses = content.split("\n").map(line => parseInt(line.trim()));
-  console.log(masses);
+  return content.split("\n").map(line => parseInt(line.trim()));
+};
 
-  const fuelCounts = masses.map((mass) => {
-      return Math.floor(mass / 3) - 2
-  })
+const partOne = async () => {
+  const masses = await getMasses();
+  const fuelCounts = masses.map(massToFuel);
 
-  console.log(_.sum(fuelCounts))
+  console.log("Part 1 answer", _.sum(fuelCounts));
+};
+
+const partTwo = async () => {
+  const masses = await getMasses();
+  const fuelCounts = masses.map(getRecursiveFuel);
+
+  console.log("Part 2 answer", _.sum(fuelCounts));
+};
+
+const main = async () => {
+  console.log(getRecursiveFuel(100756));
+  await partOne();
+  await partTwo();
 };
 
 main();
