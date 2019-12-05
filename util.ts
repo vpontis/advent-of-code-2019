8879b11ac0cd38b082d34bbc1962833ace67dc4c
Victor Pontis <victor@pont.is>
2019-12-04 21:40:07 -0800

    Finish day 5

--- util.ts
+++ util.ts
@@ -7,3 +7,19 @@ export const rangeBetween = (from: number, to: number) => {
   const firstRange = range(to - from);
   return firstRange.map(num => num + from);
 };
+
+import * as inquirer from "inquirer";
+
+export const readLine = async (
+  question: string,
+  type: string = "number"
+): Promise<string> => {
+  const { answer } = await inquirer.prompt([
+    {
+      type,
+      name: "answer",
+      message: question
+    }
+  ]);
+  return answer;
+};
